108398eeb11a16683c2f61d602f6585639b7fcb2
suryadev777 <suryaiyappan777@gmail.com>
2023-10-01 16:17:31 +0530

    [update] update user table name and add followship table
    user table updated user->User due to standard name
    add followShip table that contains followers and following tables

--- app/database/model.py
+++ app/database/model.py
@@ -1,15 +1,34 @@
 # contains all the database schemas
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from dotenv import load_dotenv
+import os
 
-Base = DeclarativeBase()
+config = load_dotenv(".env")
+
+Base = declarative_base()
+posgresUrl = os.environ.get("POSGRESQL_URL")
+engine = create_engine(posgresUrl)
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "User"
+    # username consider as id
     id = Column(String, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     DOB = Column(Date)
     age = Column(Integer)
+
+
+class FollowShip(Base):
+    # followers and following details
+    __tablename__ = "Followship"
+    id = Column(String, primary_key=True, autoincrement=True)
+    followersId = relationship(User.id)
+    followingId = relationship(User.id)
+
+
+Base.metadata.create_all(engine)
+engine.dispose()
